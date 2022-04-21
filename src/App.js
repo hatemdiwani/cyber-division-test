65b2c231456c07eb24dbdeb35bc7cadb9c35d487
hatmendiwani <hatmen.dio@outlook.com>
2022-04-21 22:45:22 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import Container from './components/ui/Container';
 import Landing from './pages/Landing';
 function App() {
 
-
-
-  const ref = useRef()
-
+  const ref = useRef(null)
 
   const draw = (height, width) => {
-    console.log(width, height);
-    var canvas = document.getElementById('canvas');
+
+
+    var canvas = document.createElement('canvas');
     canvas.width = width;
-    canvas.height = 10000;
+    canvas.height = height;
     var ctx = canvas.getContext('2d');
     var imgData = ctx.getImageData(0, 0, width, height);
 
@@ -39,40 +37,30 @@ function App() {
 
     ctx.putImageData(imgData, 0, 0);
     document.body.appendChild(canvas);
+
   }
 
 
-
-
   useEffect(() => {
-    console.log('window inner height: ', window.innerHeight);
 
-    console.log('document Element client hieght: ', document.documentElement.clientHeight);
+    if (ref) {
+      draw(ref.current.scrollHeight, ref.current.clientWidth)
+    }
 
-    console.log('document Element scroll hieght: ', document.documentElement.scrollHeight);
+  }, [])
 
-    console.log('document Element offset height: ', document.documentElement.offsetHeight);
-
-    console.log('document element scrolltop: ', document.documentElement.scrollTop);
-
-    console.log('window page Y Offset: ', window.pageYOffset);
-
-    console.log('window document body offsetheight: ', window.document.body.offsetHeight);
-    console.log(ref.current.clientHeight);
-    console.log(ref.current.clientHeight);
-    draw(document.documentElement.scrollHeight, document.documentElement.clientWidth)
-  })
 
 
 
 
 
   return (
-    <div style={{ position: 'relative' }} ref={ref}>
-      <canvas id="canvas" style={{ position: 'absolute' }} >
-      </canvas>
-      <div style={{ zIndex: 1, position: 'absolute', width: '100%', height: '100%' }} >
-        <Landing />
+    <div  >
+
+      <div style={{ position: 'relative' }}  >
+        <div ref={ref} style={{ zIndex: 1, position: 'absolute' }} >
+          <Landing />
+        </div>
       </div>
     </div>
 
